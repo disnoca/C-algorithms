f6fa5eec3ff126310c20c83e585c7b032336fe89
disnoca <samuelp2002@gmail.com>
2023-04-28 18:02:40 +0100

    refactored merge sort

--- sorting/merge_sort/merge_sort.c
+++ sorting/merge_sort/merge_sort.c
@@ -16,29 +16,29 @@ static void merge(int* arr, int* left_arr, int left_length, int* right_arr, int 
 	
 	while(j < right_length)
 		arr[k++] = right_arr[j++];
-	
-	Free(left_arr);
-	Free(right_arr);
 }
 
 void merge_sort(int* arr, int length) {
-		if(length < 2) return;
-		
-		int mid_pos = length/2;
+	if(length < 2) return;
+	
+	int mid_pos = length/2;
 
-		int left_length = mid_pos;
-		int* left_arr = Calloc(sizeof(int), left_length);
-		int right_length = length - mid_pos;
-		int* right_arr = Calloc(sizeof(int), right_length);
-		
-		for(int i = 0; i < mid_pos; i++)
-			left_arr[i] = arr[i];
-		
-		for(int i = mid_pos; i < length; i++)
-			right_arr[i-mid_pos] = arr[i];
-		
-		merge_sort(left_arr, left_length);
-		merge_sort(right_arr, right_length);
-		
-		merge(arr, left_arr, left_length, right_arr, right_length);
+	int left_length = mid_pos;
+	int* left_arr = Calloc(sizeof(int), left_length);
+	int right_length = length - mid_pos;
+	int* right_arr = Calloc(sizeof(int), right_length);
+	
+	for(int i = 0; i < mid_pos; i++)
+		left_arr[i] = arr[i];
+	
+	for(int i = mid_pos; i < length; i++)
+		right_arr[i-mid_pos] = arr[i];
+	
+	merge_sort(left_arr, left_length);
+	merge_sort(right_arr, right_length);
+	
+	merge(arr, left_arr, left_length, right_arr, right_length);
+
+	Free(left_arr);
+	Free(right_arr);
 }
