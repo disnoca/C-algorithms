d3fcca7262b0375b6f3a60fb907d5300652bdedb
disnoca <samuelp2002@gmail.com>
2023-05-02 13:54:19 +0100

    added array list data structure

---
+++ data_structures/standard/array_list/array_list.c
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include "array_list.h"
+#include "../../../wrapper_functions.h"
+
+static void resize(ArrayList* list) {
+	int old_max_size = list->max_size;
+	list->max_size *= 2;
+	data_type* new_arr = (data_type*) Malloc(sizeof(data_type) * list->max_size);
+
+	for(int i = 0; i < old_max_size; i++)
+		new_arr[i] = list->arr[i];
+	
+	Free(list->arr);
+	list->arr = new_arr;
+}
+
+/* Helper Functions */
+
+/* Header Implementations */
+
+ArrayList* al_create(int size) {
+	ArrayList* list =  (ArrayList*) Malloc(sizeof(ArrayList));
+
+	list->arr = (data_type*) Malloc(sizeof(data_type) * size);
+	list->curr_size = 0;
+	list->max_size = size;
+
+	return list;
+}
+
+void al_add(ArrayList* list, data_type data) {
+	if(list->curr_size >= list->max_size)
+		resize(list);
+
+	list->arr[list->curr_size++] = data;
+}
+
+void al_add_at(ArrayList* list, data_type data, int pos) {
+	if(pos == list->curr_size) {
+		al_add(list, data);
+		return;
+	}
+
+	if(list->curr_size == list->max_size)
+		resize(list);
+
+	for(int i = list->curr_size; i > pos; i--)
+		list->arr[i] = list->arr[i-1];
+
+	list->arr[pos] = data;
+	list->curr_size++;
+}
+
+data_type al_remove_last(ArrayList* list) {
+	return list->arr[--list->curr_size];
+}
+
+data_type al_remove_at(ArrayList* list, int pos) {
+	if(pos == list->curr_size-1)
+		return al_remove_last(list);
+
+	data_type data = list->arr[pos];
+
+	list->curr_size--;
+	for(int i = pos; i < list->curr_size; i++)
+		list->arr[i] = list->arr[i+1];
+
+	return data;
+}
+
+bool al_remove(ArrayList* list, data_type data) {
+	int pos = al_index_of(list, data);
+	if(pos == -1)
+		return false;
+
+	al_remove_at(list, pos);
+	return true;
+}
+
+data_type al_get(ArrayList* list, int pos) {
+	return list->arr[pos];
+}
+
+data_type al_set(ArrayList* list, data_type data, int pos) {
+	data_type old_data = list->arr[pos];
+	list->arr[pos] = data;
+	return old_data;
+}
+
+int al_index_of(ArrayList* list, data_type data) {
+	for(int i = 0; i < list->curr_size; i++)
+		if(list->arr[i] == data)
+			return i;
+
+	return -1;
+}
+
+bool al_contains(ArrayList* list, data_type data) {
+	return al_index_of(list, data) != -1;
+}
+
+void al_clear(ArrayList* list) {
+	list->curr_size = 0;
+}
+
+void al_destroy(ArrayList* list) {
+	Free(list->arr);
+	Free(list);
+}
---
+++ data_structures/standard/array_list/array_list.h
@@ -0,0 +1,39 @@
+#ifndef ARRAY_LIST
+#define ARRAY_LIST
+
+typedef int data_type;
+
+/* ---------------- Structs ---------------- */
+
+typedef struct {
+	data_type* arr;
+	int curr_size, max_size;
+} ArrayList;
+
+/* ---------------- Functions ---------------- */
+
+ArrayList* al_create();
+
+void al_add(ArrayList* list, data_type data);
+
+void al_add_at(ArrayList* list, data_type data, int pos);
+
+data_type al_remove_last(ArrayList* list);
+
+data_type al_remove_at(ArrayList* list, int pos);
+
+bool al_remove(ArrayList* list, data_type data);
+
+data_type al_get(ArrayList* list, int pos);
+
+data_type al_set(ArrayList* list, data_type data, int pos);
+
+int al_index_of(ArrayList* list, data_type data);
+
+bool al_contains(ArrayList* list, data_type data);
+
+void al_clear(ArrayList* list);
+
+void al_destroy(ArrayList* list);
+
+#endif
